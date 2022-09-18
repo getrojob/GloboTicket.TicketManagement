0fc6594397d96754aee4d650cff7f529d380bf01
getrojob <getrojob@gmai.com>
2022-09-18 13:02:15 -0300

    Feat: add Validation

--- src/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ src/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -1,10 +1,14 @@
 ﻿using FluentValidation;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
     {
+        private readonly IEventRepository _eventRepository;
         public CreateEventCommandValidator()
         {
             RuleFor(p => p.Name)
@@ -17,9 +21,18 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
                 .NotNull()
                 .GreaterThan(DateTime.Now);
 
+            RuleFor(e => e)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and date already exists.");
+
             RuleFor(p => p.Price)
                 .NotEmpty().WithMessage("{PropertyName} is required")
                 .GreaterThan(0);
+        }
+
+        private async Task<bool> EventNameAndDateUnique(CreateEventCommand e, CancellationToken token)
+        {
+            return !(await _eventRepository.IsEventNameAndDateUnique(e.Name, e.Date));
         }
     }
 }
