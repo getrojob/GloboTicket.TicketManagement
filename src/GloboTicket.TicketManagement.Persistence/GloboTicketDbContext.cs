b150332b14e678399e4befcfe646a020ddb183c5
getrojob <getrojob@gmai.com>
2022-09-19 19:55:44 -0300

    Feat: Add project Persistence and DbContext

---
+++ src/GloboTicket.TicketManagement.Persistence/GloboTicketDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Persistence
+{
+    public class GloboTicketDbContext : DbContext
+    {
+        public GloboTicketDbContext(DbContextOptions<GloboTicketDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
